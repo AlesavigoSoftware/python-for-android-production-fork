29959c4b2cd022f51b12bd9e25fa3ab5fecf486f
Aleksandr <56115945+AlSavIg@users.noreply.github.com+alesavigo@gmail.com>
2024-03-19 09:55:57 +0300

    Модификация requirements

--- pythonforandroid/recipes/agro_uav_api/__init__.py
+++ pythonforandroid/recipes/agro_uav_api/__init__.py
@@ -3,31 +3,32 @@ from pathlib import Path
 from typing import Union
 
 
-class ProjectPathManager:
-    PROJECT_NAME = "agro-uav-app"
-
-    _dir_path = None
-
-    @classmethod
-    def get_project_dir_path(cls) -> Path:
-        if cls._dir_path is None:
-
-            cls._dir_path = Path()
-            for folder in Path(__file__).absolute().parts:
-
-                cls._dir_path = cls._dir_path.joinpath(folder)
-                if folder == cls.PROJECT_NAME:
-                    break
-
-        return cls._dir_path
-
-    @classmethod
-    def join_str_paths(cls, *args: Union[Path, str]) -> str:
-        return str(Path().joinpath(*args))
+# class ProjectPathManager:
+#     PROJECT_NAME = "agro-uav-app"
+#
+#     _dir_path = None
+#
+#     @classmethod
+#     def get_project_dir_path(cls) -> Path:
+#         if cls._dir_path is None:
+#
+#             cls._dir_path = Path()
+#             for folder in Path(__file__).absolute().parts:
+#
+#                 cls._dir_path = cls._dir_path.joinpath(folder)
+#                 if folder == cls.PROJECT_NAME:
+#                     break
+#
+#         return cls._dir_path
+#
+#     @classmethod
+#     def join_str_paths(cls, *args: Union[Path, str]) -> str:
+#         return str(Path().joinpath(*args))
 
 
 class AgroUAVAPIRecipe(PythonRecipe):
-    url = ProjectPathManager.join_str_paths(ProjectPathManager.get_project_dir_path(), 'Libs', 'agro-uav-api')
+    url = 'git+ssh:/0/0g0it@github.com/AlesavigoSoftware/agro-uav-api.git'
+    # url = ProjectPathManager.join_str_paths(ProjectPathManager.get_project_dir_path(), 'Libs', 'agro-uav-api')
 
     # call_hostpython_via_targetpython = False
     install_in_hostpython = False
--- pythonforandroid/recipes/agro_uav_missionkit/__init__.py
+++ pythonforandroid/recipes/agro_uav_missionkit/__init__.py
@@ -1,53 +1,36 @@
-from pythonforandroid.recipe import PythonRecipe
-from pathlib import Path
-from typing import Union
-from os.path import basename, dirname, exists, isdir, isfile, join, realpath, split
-import glob
-
-import hashlib
-from re import match
-
 import sh
-import shutil
-import fnmatch
-import urllib.request
-from urllib.request import urlretrieve
-from os import listdir, unlink, environ, curdir, walk
-from sys import stdout
-import time
 
-import packaging.version
-
+from pythonforandroid.recipe import PythonRecipe
 from pythonforandroid.logger import logger, info, warning, debug, shprint, info_main
 from pythonforandroid.util import current_directory, ensure_dir, BuildInterruptingException, rmdir, move, touch
-from pythonforandroid.util import load_source as import_recipe
 
 
-class ProjectPathManager:
-    PROJECT_NAME = "agro-uav-app"
-
-    _dir_path = None
-
-    @classmethod
-    def get_project_dir_path(cls) -> Path:
-        if cls._dir_path is None:
-
-            cls._dir_path = Path()
-            for folder in Path(__file__).absolute().parts:
-
-                cls._dir_path = cls._dir_path.joinpath(folder)
-                if folder == cls.PROJECT_NAME:
-                    break
-
-        return cls._dir_path
-
-    @classmethod
-    def join_str_paths(cls, *args: Union[Path, str]) -> str:
-        return str(Path().joinpath(*args))
+# class ProjectPathManager:
+#     PROJECT_NAME = "agro-uav-app"
+#
+#     _dir_path = None
+#
+#     @classmethod
+#     def get_project_dir_path(cls) -> Path:
+#         if cls._dir_path is None:
+#
+#             cls._dir_path = Path()
+#             for folder in Path(__file__).absolute().parts:
+#
+#                 cls._dir_path = cls._dir_path.joinpath(folder)
+#                 if folder == cls.PROJECT_NAME:
+#                     break
+#
+#         return cls._dir_path
+#
+#     @classmethod
+#     def join_str_paths(cls, *args: Union[Path, str]) -> str:
+#         return str(Path().joinpath(*args))
 
 
 class AgroUAVMissionKitRecipe(PythonRecipe):
-    url = ProjectPathManager.join_str_paths(ProjectPathManager.get_project_dir_path(), 'Libs', 'agro-uav-missionkit')
+    url = 'git+ssh://git@github.com/AlesavigoSoftware/agro-uav-missionkit.git'
+    # url = ProjectPathManager.join_str_paths(ProjectPathManager.get_project_dir_path(), 'Libs', 'agro-uav-missionkit')
 
     depends = [
         'setuptools',
