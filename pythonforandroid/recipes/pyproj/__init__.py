344a9e2014e01e58afd3d9dd98d3ee6986c2318f
alesavigo <alesavigo@gmail.com>
2025-12-01 20:31:28 +0300

    Updated pyproj recipe to 3.7.2

--- pythonforandroid/recipes/pyproj/__init__.py
+++ pythonforandroid/recipes/pyproj/__init__.py
@@ -2,9 +2,9 @@ from pythonforandroid.recipe import CythonRecipe
 
 
 class PyProjRecipe(CythonRecipe):
-    version = 'v2.1.0rel'
-    url = 'https://github.com/pyproj4/pyproj/archive/refs/tags/{version}.tar.gz'
-    depends = ['setuptools']
+    version = "3.7.2"
+    url = "https://github.com/pyproj4/pyproj/archive/refs/tags/{version}.tar.gz"
+    depends = ["setuptools>=61.0.0", "wheel", "cython>=3.1"]
     call_hostpython_via_targetpython = False
 
 
